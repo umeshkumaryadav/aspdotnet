19bcf6fe9fd3744bda33add5e49e0333e809daf7
umeshkumaryadav <iumeshpro@gmail.com>
2021-08-29 09:29:46 +0530

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -10,12 +10,12 @@ class Header extends Component {
                <nav className="navbar navbar-inverse">
                 <div className="container-fluid">
                     <div className="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                         <span className="icon-bar"></span>
                         <span className="icon-bar"></span>
                         <span className="icon-bar"></span>                        
                     </button>
-                    <a class="navbar-brand">Developer Funnel</a>
+                    <a className="navbar-brand">Developer Funnel</a>
                     </div>
                     <div className="collapse navbar-collapse" id="myNavbar">
                     <ul className="nav navbar-nav">
@@ -23,9 +23,9 @@ class Header extends Component {
                         <li><a >Page 2</a></li>
                         <li><a>Page 3</a></li>
                     </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                        <li><a ><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <ul className="nav navbar-nav navbar-right">
+                        <li><a><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                        <li><a ><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
                     </ul>
                     </div>
                 </div>
