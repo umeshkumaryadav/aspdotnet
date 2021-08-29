bcdc6ac0ed58407757ec6dd629322accbe8e0c1f
umeshkumaryadav <iumeshpro@gmail.com>
2021-08-29 09:24:13 +0530

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -7,9 +7,9 @@ class Header extends Component {
       
         return(
             <Fragment>
-               <nav class="navbar navbar-inverse">
-                <div class="container-fluid">
-                    <div class="navbar-header">
+               <nav className="navbar navbar-inverse">
+                <div className="container-fluid">
+                    <div className="navbar-header">
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
